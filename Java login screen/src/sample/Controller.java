8d00793007134ef3bbd55775a9fdc6ed6fd65a23
Teodorant1 <52867608+Teodorant1@users.noreply.github.com>
2020-07-16 18:01:34 +0200

    Initial commit

---
+++ Java login screen/src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+    public void setUsername(KeyEvent keyEvent) {
+    }
+
+    public void shizzlesticks(MouseEvent mouseEvent) {
+    }
+
+    public void setPassword(KeyEvent keyEvent) {
+    }
+}
